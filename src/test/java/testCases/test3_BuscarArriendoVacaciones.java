bbb1f6f329ab83ffcfe4f5203580138def710029
Jose Miguel Ronco S <jose.ronco@externos.bci.cl>
2021-03-06 22:26:25 -0300

    ultimos arreglos

--- src/test/java/testCases/test3_BuscarArriendoVacaciones.java
+++ src/test/java/testCases/test3_BuscarArriendoVacaciones.java
@@ -6,7 +6,6 @@ import page.Login;
 
 public class test3_BuscarArriendoVacaciones {
 
-<<<<<<< HEAD
     String busqueda = "arriendo vacaciones";
 
     @Test
@@ -14,7 +13,4 @@ public class test3_BuscarArriendoVacaciones {
         BuscarArriendo buscarArriendo = new BuscarArriendo();
         buscarArriendo.busquedaArriendoVacaciones(busqueda);
     }
-=======
-
->>>>>>> 6b51fa451d0a79f745d795d30cc59b23ab56ed6f
 }
